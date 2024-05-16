729a7ee55bca5ca34e80ab0b37ad0c708776142b
run-node <160773499+run-node@users.noreply.github.com>
2024-05-17 03:44:10 +0800

    Create Quili.sh

---
+++ Quili.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# 检查是否以root用户运行脚本
+if [ "$(id -u)" != "0" ]; then
+    echo "此脚本需要以root用户权限运行。"
+    echo "请尝试使用 'sudo -i' 命令切换到root用户，然后再次运行此脚本。"
+    exit 1
+fi
+
+# 脚本保存路径
+SCRIPT_PATH="$HOME/Quilibrium.sh"
+
+# 节点安装功能
+function install_node() {
+
+# 增加swap空间
+sudo mkdir /swap
+sudo fallocate -l 24G /swap/swapfile
+sudo chmod 600 /swap/swapfile
+sudo mkswap /swap/swapfile
+sudo swapon /swap/swapfile
+echo '/swap/swapfile swap swap defaults 0 0' >> /etc/fstab
+
+# 向/etc/sysctl.conf文件追加内容
+echo -e "\n# 自定义最大接收和发送缓冲区大小" >> /etc/sysctl.conf
+echo "net.core.rmem_max=600000000" >> /etc/sysctl.conf
+echo "net.core.wmem_max=600000000" >> /etc/sysctl.conf
+
+echo "配置已添加到/etc/sysctl.conf"
+
+# 重新加载sysctl配置以应用更改
+sysctl -p
+
+echo "sysctl配置已重新加载"
+
+# 更新并升级Ubuntu软件包
+sudo apt update && sudo apt -y upgrade 
+
+# 安装wget、screen和git等组件
+sudo apt install git ufw bison screen binutils gcc make bsdmainutils -y
+
+sudo apt install golang-go
+go version
+
+# 安装GVM
+bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source /root/.gvm/scripts/gvm
+
+gvm install go1.4 -B
+gvm use go1.4
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.17.13
+gvm use go1.17.13
+export GOROOT_BOOTSTRAP=$GOROOT
+gvm install go1.20.2
+gvm use go1.20.2
+
+# 克隆仓库
+git clone https://github.com/quilibriumnetwork/ceremonyclient
+
+# 进入ceremonyclient/node目录
+cd ceremonyclient/node 
+
+# 赋予执行权限
+chmod +x poor_mans_cd.sh
+
+# 创建一个screen会话并运行命令
+screen -dmS Quili bash -c './poor_mans_cd.sh'
+
+}
+
+
+# 查看节点日志
+function check_service_status() {
+    screen -r Quili
+   
+}
+
+function check_address(){
+source /root/.gvm/scripts/gvm
+gvm use go1.20.2
+cd ~/ceremonyclient/node/ && GOEXPERIMENT=arenas go run ./... -peer-id
+}
+
+function restart(){
+screen -X -S Quili quit
+source /root/.gvm/scripts/gvm
+gvm use go1.20.2
+cd ceremonyclient/node
+# 创建一个screen会话并运行命令
+screen -dmS Quili bash -c './poor_mans_cd.sh'
+}
+
+function backup(){
+cp $HOME/ceremonyclient/node/.config/{config.yml,keys.yml} $HOME
+}
+
+# 主菜单
+function main_menu() {
+    clear
+    echo "==========================自用脚本=============================="
+    echo "需要测试网节点部署托管 技术指导 部署领水质押脚本 请联系Telegram :https://t.me/linzeusasa"
+    echo "需要测试网节点部署托管 技术指导 部署领水质押脚本 请联系Wechat :llkkxx001"
+    echo "安装后请备份您的钱包文件，路径为/root/ceremonyclient/node/.config中的config和keys两个文件"
+    echo "查询余额官网：https://quilibrium.com/"
+    echo "请选择要执行的操作:"
+    echo "1. 安装节点"
+    echo "2. 查看节点日志（查看完请按Ctrl+A后按D退出Screen）"
+    echo "3. 查询钱包地址"
+    echo "4. 重启节点（执行后请勿随意Ctrl+C中止程序）"
+    echo "5. 备份钱包文件到root目录中"
+    read -p "请输入选项（1-5）: " OPTION
+
+    case $OPTION in
+    1) install_node ;;
+    2) check_service_status ;; 
+    3) check_address ;;
+    4) restart ;;
+    5) backup ;;
+    *) echo "无效选项。" ;;
+    esac
+}
+
+# 显示主菜单
+main_menu
