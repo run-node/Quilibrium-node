8caf601a18dad5f63412139502ec02bee1b4f787
run-node <160773499+run-node@users.noreply.github.com>
2024-07-06 21:38:30 +0800

    Update Quilibrium.sh

--- Quilibrium.sh
+++ Quilibrium.sh
@@ -105,7 +105,7 @@ function check_service_status() {
 }
 
 function check_address(){
-cd ~/ceremonyclient/node && ./node-1.4.19-linux-amd64 -peer-id
+cd ~/ceremonyclient/node && ./node-1.4.21-linux-amd64 -peer-id
 
 }
 
