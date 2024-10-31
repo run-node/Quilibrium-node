99d9766517ed02b9082e54c48a8b4a8ed7dc8979
run-node <160773499+run-node@users.noreply.github.com>
2024-11-01 05:54:34 +0800

    Update restart2.sh

--- restart2.sh
+++ restart2.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# 发送 SIGTERM 信号终止指定的进程
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Terminating process node-2.0.2.3-linux-amd64..."
+pkill -SIGTERM -f node-2.0.2.3-linux-amd64
+
+# 等待10秒
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Waiting for 10 seconds..."
+sleep 10
+
+# 关闭所有匹配条件的 screen 会话
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Closing detached screen sessions..."
+screen -ls | grep Detached | grep Qui | awk -F '[.]' '{print $1}' | xargs -I {} screen -S {} -X quit
+
+# 启动新的 screen 会话并执行指定命令
+echo "[$(date +"%Y-%m-%d %H:%M:%S")] Starting new screen session for Quili..."
+> /root/screen_log.txt && screen -L -Logfile /root/screen_log.txt -dmS Quili bash -c 'cd ~/ceremonyclient/node && ./node-2.0.2.3-linux-amd64'
+    
 # 无限循环
 while true; do
     # 初始化上一个 ts 和 increment 值
